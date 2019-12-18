eddfcbbc29e3b6b6eac217ce96ca9de6813ee8cb
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-17 20:37:18 -0600

    added myAnswer to question 3

--- joins-solution.sql
+++ joins-solution.sql
@@ -7,3 +7,9 @@ ORDER BY "customers"."id";
 SELECT "products"."description", "line_items"."quantity", "line_items"."order_id" FROM "line_items"
 JOIN "products" on "products"."id" = "line_items"."product_id"
 ORDER BY "line_items"."order_id";
+
+-- 3. Which warehouses have cheetos
+SELECT "warehouse", "products"."description" FROM "warehouse_product"
+JOIN "products" on "products"."id" = "warehouse_product"."product_id"
+JOIN "warehouse" on "warehouse"."id" = "warehouse_product"."warehouse_id"
+WHERE "product_id" = 5;
